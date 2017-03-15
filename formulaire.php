fcc003737f5a7f7623d3d53a0af0cfe3799932b8
Quentin Grau <q.grau00@gmail.com>
2017-03-15 11:46:58 +0100

    ajout message

--- formulaire.php
+++ formulaire.php
@@ -1,37 +1,37 @@
 <?php
-	$erreur = [];
+	$message = [];
 
 if (isset($_POST) && !empty($_POST)){
 	$donne=[];
 	if (isset($_POST)['nom']) && $_POST['nom']!=''){
-		$donne[] = $_POST['nom'];
+		$donne['lastName'] = $_POST['nom'];
 	}else{
-		$erreur[] ='merci de mettre un nom'
+		$message[] ='merci de mettre un nom'
 		}
 	if (isset($_POST)['prenom']) && $_POST['prenom']!='') {
-		$donne[] = $_POST['prenom'];
+		$donne['firstName'] = $_POST['prenom'];
 	}else{
-		$erreur[] ='merci de mettre un prenom'
+		$message[] ='merci de mettre un prenom'
 		}
 	if (isset($_POST)['naissance'])&& $_POST['prenom']!='') {
-		$donne[] =$_POST['naissance'];
+		$donne['birthDate'] =$_POST['naissance'];
 	}else{
-		$erreur[] ='merci de mettre une date de naissance'
+		$message[] ='merci de mettre une date de naissance'
 		}
 	if (isset($_POST)['card'])) {
-		$donne[] =  0;
-		$donne[] = null;
+		$donne['card'] =  0;
 
 		if (isset($_POST['numeCard'])) {
-			$numeCard = $_POST['numeCard'];
+			$donne['cardNumber'] = $_POST['numeCard'];
 		}else{
-			$erreur[]='merci de mettre un numéro de carte';
+			$message[]='merci de mettre un numéro de carte';
 		}
 	}else{
-		$card = 0
+		$donne['card'] = 0
+		$donne['cardNumber'] = null;
 	}
 
-	if (empty($erreur)){
+	if (empty($message)){
 		$pdo = new PDO ('mysql:host=localhost;dbname=colyseum;charset=utf8', 'root', '');
 		$pdo -> setAttribute(
 			PDO::ATTR_ERRMODE, 
@@ -48,9 +48,7 @@ if (isset($_POST) && !empty($_POST)){
 			  cardNumber = :cardNumber,
 			  ;)
     	$statement->execute($donne);
-    	$erreur[]='le client est bien ajouté';
-
-
+    	$message[]='le client est bien ajouté';
 	}
 	echo "post effectué";
 }
@@ -63,8 +61,8 @@ if (isset($_POST) && !empty($_POST)){
 </head>
 <body>
 <h1> ajout de client </h1>
-<?php foreach ($erreur as $value) {
-	echo "<li>'$value'</li>";
+<?php foreach ($message as $key => $value) {
+	echo "<li class="$key">'$value'</li>";
 }
 ?>
 <form method="post" action="">
