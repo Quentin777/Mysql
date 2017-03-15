09f3748737209e4752bea6492cab30d11ee18fbd
Quentin Grau <q.grau00@gmail.com>
2017-03-15 15:05:42 +0100

    maj

--- exo3.php
+++ exo3.php
@@ -1,10 +1,33 @@
 <?php
-	$message = [];
+$pdo = new PDO('mysql:dbname=colyseum;host=localhost; charset=utf8', 'root', '');
+$pdo->setAttribute(
+	PDO::ATTR_ERRMODE, 
+	PDO::ERRMODE_EXCEPTION);
+$pdo->setAttribute(
+	PDO::ATTR_DEFAULT_FETCH_MODE, 
+	PDO::FETCH_OBJ);
 
+$statement =$pdo->query('SELECT id, type FROM cardTypes');
+$typedecarte = $statement-> fetchall();
+//echo "<pre>";
+//var_dump($typedecarte);
+//echo "<pre>";
+//die()
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FORMAULAIRE</title>
+		<link rel="stylesheet" type="text/css" href="style/style.css">
+	
+</head>
+<body>
+<?php
+$erreur = [];
 if (isset($_POST) && !empty($_POST)) {
 	$donner=[];
-
-	if (isset($_POST["nom"]) && $_POST['nom']!='') {
+		if (isset($_POST["nom"]) && $_POST['nom']!='') {
 			$donner['lastname'] = $_POST["nom"];
 		}else{
 			$erreur[] = 'merci de mettre un nom';
@@ -27,12 +50,13 @@ if (isset($_POST) && !empty($_POST)) {
 				}else{
 				$erreur[] = 'merci de mettre un numéro carte';
 				}
+				if 
 		}else{
 			$donner['card'] = 0;
 			$donner['cardNumber'] = null;
 		}
 
-if (empty($erreur)) {
+		if (empty($erreur)) {
 			/*INSERT TO INTO nomdelatable SET*/
 			$statement = $pdo->prepare("
 				INSERT INTO clients
@@ -41,6 +65,12 @@ if (empty($erreur)) {
 				birthDate = :birthDate,
 				card = :card,
 				cardNumber = :cardNumber");
+
+			$statement = $pdo->prepare("
+				INSERT INTO card
+				SET cardNumber = :cardNumber,
+				cardTypesId = :cardTypesId,
+				");
 /*			echo "<pre>";
 var_dump($donner);*/
 			$statement->execute($donner);
@@ -48,33 +78,43 @@ $erreur[] = "<div class='list-group-item list-group-item-success'>le client est 
 			
 		}
 	}
+/*if (isset($_POST["name"],$_POST["prenom"],$_POST["naissance"],$_POST["card"])) {
+	$nom = $_POST["name"];
+	$prenom = $_POST["prenom"];
+	$card = $_POST["card"];
+	echo "post effectué";
+}*/
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Formulaire client</title>
-	<link rel="stylesheet" type="text/css" href="style/style.css">
-</head>
-<body>
-<h1> ajout de client </h1>
-<ul>
+<h1> Ajout de client </h1>
+<nav class="navbar navbar-inverse">
+  ...
+</nav>
 
-<?php foreach ($message as $key => $tableau) {
-		foreach ($erreur as $value) {
-			echo "<li class='list-group-item list-group-item-danger'> $value <li> <br>" ;
-	}
+
+
+
+
+<?php foreach ($erreur as $value) {
+	echo "<li class='list-group-item list-group-item-danger'> $value <li> <br>" ;
 }
 ?>
-</ul>
+
 <form method="post" action="">
 	<input type="text" name="nom" placeholder="nom">
 	<input type="text" name="prenom" placeholder="prenom">
 	<input type="date" name="naissance">
-	<label for="card"> le client a-t'ils une carte de fidélité</label> <!-- pour mettre un texte devant checkbox-->
+	<label for="card"> le client veut il une carte de fidélité</label> <!-- pour mettre un texte devant checkbox-->
 	<input type="checkbox" name="card" id="card">
+	<FORM>
+		<select type="typedecarte" name="typecarte" size="1">
+		<<?php foreach ($typedecarte as $value) {
+			echo '<OPTION valeur="'.$value->id.'">'.$value->type.'</OPTION>';
+		} 
+		?>
+		</SELECT>
+	</FORM>
 	<input type="number" name="numeCard" placeholder="numéro de la carte">
 	<button type="submit">Ok</button>
 </form>
-
 </body>
 </html>
---
+++ index2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+
+<a href="index.php">exo1</a>
+<a href="formulaire.php">exo2</a>
+<a href="exo3.php">exo3</a>
+</body>
+</html>
